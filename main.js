9431d654bfb5491464cd2578cb07749779cb9916
chirva-ivan <pooeblos@gmail.com>
2016-06-01 21:47:51 +0300

    Add user info

---
+++ main.js
@@ -0,0 +1,73 @@
+app.controller("mainController", ["$scope", "Auth", "UserRef", "Messages", function($scope, Auth, UserRef, Messages) {
+
+  $scope.createUser = function () {
+    Auth.$createUser({
+    	email: $scope.emailNew,
+       	password: $scope.passwordNew
+     	}).then(function(userData) {
+  		
+      //additional information for new user
+      var userInfo = {
+    		key: userData.uid,
+    		date: Firebase.ServerValue.TIMESTAMP,
+    		username: $scope.usernameNew,
+    		email: $scope.emailNew
+    	};
+  		       
+  	UserRef.child(userData.uid).set(userInfo);
+
+   	console.log("User created: " + userData.uid);
+    	$scope.usernameNew = '';
+    	$scope.emailNew = '';
+    	$scope.passwordNew = '';
+  	});
+  };
+
+    //temporary function for getting data
+  $scope.get = function () {  
+  	console.log($scope.currentUser);
+      UserRef.on("value", function(snapshot) {
+        console.log(snapshot.val());
+      });
+  	console.log(Messages);
+  };
+    
+  $scope.login = function () {
+  	Auth.$authWithPassword({
+  		email: $scope.email,
+  		password: $scope.password
+    }).then(function(authData) {
+    	$scope.email = '';
+    	$scope.password = '';
+    	$scope.currentUser = authData;
+
+    	var ref = new Firebase("https://blinding-torch-9498.firebaseio.com/users/" + authData.uid + "/");
+    		
+    	ref.on("value", function(snapshot) {				
+    		$scope.currentUser.userInfo = snapshot.val()
+    	});
+    }), function() {
+  		remember: "sessionOnly"
+  	};
+  };
+
+  $scope.sendMessage = function () {
+  	var currentdate = new Date();
+  	
+  	Messages.$add({
+  		content: $scope.messagePost,
+  		author: $scope.currentUser.userInfo.username,
+  		date:	currentdate.getHours() + ":" + currentdate.getMinutes() + ":"  + currentdate.getSeconds()
+  	});
+
+  	$scope.messagePost = '';
+  };
+
+  $scope.messages = Messages;
+
+  $scope.logout = function () {
+  	$scope.currentUser = null;
+    Auth.$unauth();
+  }
+  
+}]);
---
+++ service.js
@@ -0,0 +1,21 @@
+var app = angular.module("messageApp", ["firebase"]);
+
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://blinding-torch-9498.firebaseio.com/");
+    return $firebaseAuth(ref);
+  }
+]);
+
+app.factory("UserRef", function() {
+    var ref = new Firebase("https://blinding-torch-9498.firebaseio.com/users/");
+    return ref;
+  }
+);
+
+app.factory("Messages", ["$firebaseArray",
+  function($firebaseArray) {
+    var ref = new Firebase("https://blinding-torch-9498.firebaseio.com/messages/");
+    return $firebaseArray(ref);
+  }
+]);
